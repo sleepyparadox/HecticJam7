afe86ddd67d38716cb1fac6a5863c09677b22408
don logan <sleepyparadox@gmail.com>
2015-08-14 20:31:57 +1000

    created scene with correct resolution

---
+++ Src/Hectic7/Hectic7/Unity/Assets/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Hectic7.Unity.Assets
+{
+    public class Main : MonoBehaviour
+    {
+
+    }
+}
